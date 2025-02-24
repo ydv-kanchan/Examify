d78e45c3d09fa198adddf838855bb629d34809a4
Jasjeet <jasjeet13.saini@gmail.com>
2025-02-24 10:48:00 +0530

    navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import AboutUs from "./Components/AboutUs";
 
-export default function App() {
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp_Login from "./Components/Signup_Login";
 import SignUpSelection from "./Components/SignUpSelection";
@@ -13,18 +12,16 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/about" element={<AboutUs />} />
-      </Routes>
-    </div>
     <Router>
       <Routes>
         <Route path="/" element={<SignUp_Login />} />
         <Route path="/signup-selection" element={<SignUpSelection />} />
         <Route path="/signup/user" element={<CustomerSignup />} />
         <Route path="/signup/vendor" element={<VendorSignup />} />{" "}
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </Router>
+    </div>
   );
 }
 
