a311d3470b4397924b4e1c812afb62b0aaa0225a
ydv-kanchan <ydv.kanchan24@gmail.com>
2025-02-26 22:56:28 +0530

    minor changes

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -4,20 +4,26 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="w-full h-16 flex justify-center items-center bg-white shadow-lg">
-      <div className="w-full h-full flex items-center px-16 bg-white shadow-md rounded-lg">
+      <div className="w-full h-full flex items-center px-16 bg-white shadow-md ">
         <div className="h-24 w-24 flex items-center">
-          <img 
-            src="/logo.png" 
-            alt="Logo" 
+          <img
+            src="/logo.png"
+            alt="Logo"
             className="h-full w-full object-contain"
           />
         </div>
 
         <div className="h-full flex items-center w-auto ml-auto space-x-6">
-          <Link to="/about" className="text-black font-extrabold text-l hover:text-gray-600">
+          <Link
+            to="/about"
+            className="text-black font-extrabold text-l hover:text-gray-600"
+          >
             About Us
           </Link>
-          <Link to="/contact" className="text-black font-extrabold text-l hover:text-gray-600">
+          <Link
+            to="/contact"
+            className="text-black font-extrabold text-l hover:text-gray-600"
+          >
             Contact Us
           </Link>
         </div>
